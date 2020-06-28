6b1e4775700a6af46e4d01c5a70e5fb89266f45e
Joona Hietala <joonah96@gmail.com>
2020-06-28 17:30:58 +0300

    Add model download link and change UI

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1a_yoYURA_PzPLv0RmhO9ZLH0uyXXT64g/view?usp=sharing'
+export_file_name = 'mushroom-detector.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['amanita_muscaria','amanita_virosa','cantharellus_cibarius','craterellus_cornucopioides','lactarius_torminosus']
 path = Path(__file__).parent
 
 app = Starlette()
